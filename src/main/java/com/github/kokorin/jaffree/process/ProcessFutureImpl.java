cffb169635aa802f351ecd6f5ef0f81e21df35d9
Alex Katlein <dev@vemilyus.com>
2021-01-12 00:19:45 +0100

    ProcessFuture now also implements CompletionStage
    - for Kotlin coroutines interop ;)
    - ProcessRunner runs the async task using CompletableFuture using the ForkJoinPool executor
    - need to sneakyThrow the InterruptedException, for reasons

--- src/main/java/com/github/kokorin/jaffree/process/ProcessFutureImpl.java
+++ src/main/java/com/github/kokorin/jaffree/process/ProcessFutureImpl.java
@@ -18,27 +18,33 @@
 package com.github.kokorin.jaffree.process;
 
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 class ProcessFutureImpl<V> implements ProcessFuture<V> {
-    private final Future<V> delegate;
+    private final CompletableFuture<V> delegate;
     private final ProcessAccess processAccess;
-    
-    public ProcessFutureImpl(Future<V> delegate, ProcessAccess processAccess) {
+
+    ProcessFutureImpl(CompletableFuture<V> delegate, ProcessAccess processAccess) {
         Objects.requireNonNull(delegate, "delegate must not be null");
-        
+
         this.delegate = delegate;
         this.processAccess = processAccess;
     }
-    
+
     @Override
     public ProcessAccess getProcessAccess() {
         return processAccess;
     }
-    
+
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
         if (mayInterruptIfRunning) {
@@ -46,27 +52,217 @@ class ProcessFutureImpl<V> implements ProcessFuture<V> {
         } else {
             getProcessAccess().stopGracefully();
         }
-        
+
         return delegate.cancel(mayInterruptIfRunning);
     }
-    
+
     @Override
     public boolean isCancelled() {
         return delegate.isCancelled();
     }
-    
+
     @Override
     public boolean isDone() {
         return delegate.isDone();
     }
-    
+
     @Override
     public V get() throws InterruptedException, ExecutionException {
         return delegate.get();
     }
-    
+
     @Override
     public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
         return delegate.get(timeout, unit);
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenApply(Function<? super V, ? extends U> fn) {
+        return new ProcessFutureImpl<>(delegate.thenApply(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenApplyAsync(Function<? super V, ? extends U> fn) {
+        return new ProcessFutureImpl<>(delegate.thenApplyAsync(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenApplyAsync(Function<? super V, ? extends U> fn, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenApplyAsync(fn, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenAccept(Consumer<? super V> action) {
+        return new ProcessFutureImpl<>(delegate.thenAccept(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenAcceptAsync(Consumer<? super V> action) {
+        return new ProcessFutureImpl<>(delegate.thenAcceptAsync(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenAcceptAsync(Consumer<? super V> action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenAcceptAsync(action, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenRun(Runnable action) {
+        return new ProcessFutureImpl<>(delegate.thenRun(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenRunAsync(Runnable action) {
+        return new ProcessFutureImpl<>(delegate.thenRunAsync(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> thenRunAsync(Runnable action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenRunAsync(action, executor), getProcessAccess());
+    }
+
+    @Override
+    public <U, V1> ProcessFuture<V1> thenCombine(CompletionStage<? extends U> other, BiFunction<? super V, ? super U, ? extends V1> fn) {
+        return new ProcessFutureImpl<>(delegate.thenCombine(other, fn), getProcessAccess());
+    }
+
+    @Override
+    public <U, V1> ProcessFuture<V1> thenCombineAsync(CompletionStage<? extends U> other, BiFunction<? super V, ? super U, ? extends V1> fn) {
+        return new ProcessFutureImpl<>(delegate.thenCombineAsync(other, fn), getProcessAccess());
+    }
+
+    @Override
+    public <U, V1> ProcessFuture<V1> thenCombineAsync(CompletionStage<? extends U> other, BiFunction<? super V, ? super U, ? extends V1> fn, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenCombineAsync(other, fn, executor), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<Void> thenAcceptBoth(CompletionStage<? extends U> other, BiConsumer<? super V, ? super U> action) {
+        return new ProcessFutureImpl<>(delegate.thenAcceptBoth(other, action), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<Void> thenAcceptBothAsync(CompletionStage<? extends U> other, BiConsumer<? super V, ? super U> action) {
+        return new ProcessFutureImpl<>(delegate.thenAcceptBothAsync(other, action), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<Void> thenAcceptBothAsync(CompletionStage<? extends U> other, BiConsumer<? super V, ? super U> action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenAcceptBothAsync(other, action, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterBoth(CompletionStage<?> other, Runnable action) {
+        return new ProcessFutureImpl<>(delegate.runAfterBoth(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterBothAsync(CompletionStage<?> other, Runnable action) {
+        return new ProcessFutureImpl<>(delegate.runAfterBothAsync(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterBothAsync(CompletionStage<?> other, Runnable action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.runAfterBothAsync(other, action, executor), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> applyToEither(CompletionStage<? extends V> other, Function<? super V, U> fn) {
+        return new ProcessFutureImpl<>(delegate.applyToEither(other, fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> applyToEitherAsync(CompletionStage<? extends V> other, Function<? super V, U> fn) {
+        return new ProcessFutureImpl<>(delegate.applyToEitherAsync(other, fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> applyToEitherAsync(CompletionStage<? extends V> other, Function<? super V, U> fn, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.applyToEitherAsync(other, fn, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> acceptEither(CompletionStage<? extends V> other, Consumer<? super V> action) {
+        return new ProcessFutureImpl<>(delegate.acceptEither(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> acceptEitherAsync(CompletionStage<? extends V> other, Consumer<? super V> action) {
+        return new ProcessFutureImpl<>(delegate.acceptEitherAsync(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> acceptEitherAsync(CompletionStage<? extends V> other, Consumer<? super V> action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.acceptEitherAsync(other, action, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterEither(CompletionStage<?> other, Runnable action) {
+        return new ProcessFutureImpl<>(delegate.runAfterEither(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterEitherAsync(CompletionStage<?> other, Runnable action) {
+        return new ProcessFutureImpl<>(delegate.runAfterEitherAsync(other, action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<Void> runAfterEitherAsync(CompletionStage<?> other, Runnable action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.runAfterEitherAsync(other, action, executor), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenCompose(Function<? super V, ? extends CompletionStage<U>> fn) {
+        return new ProcessFutureImpl<>(delegate.thenCompose(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenComposeAsync(Function<? super V, ? extends CompletionStage<U>> fn) {
+        return new ProcessFutureImpl<>(delegate.thenComposeAsync(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> thenComposeAsync(Function<? super V, ? extends CompletionStage<U>> fn, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.thenComposeAsync(fn, executor), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> handle(BiFunction<? super V, Throwable, ? extends U> fn) {
+        return new ProcessFutureImpl<>(delegate.handle(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> handleAsync(BiFunction<? super V, Throwable, ? extends U> fn) {
+        return new ProcessFutureImpl<>(delegate.handleAsync(fn), getProcessAccess());
+    }
+
+    @Override
+    public <U> ProcessFuture<U> handleAsync(BiFunction<? super V, Throwable, ? extends U> fn, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.handleAsync(fn, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<V> whenComplete(BiConsumer<? super V, ? super Throwable> action) {
+        return new ProcessFutureImpl<>(delegate.whenComplete(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<V> whenCompleteAsync(BiConsumer<? super V, ? super Throwable> action) {
+        return new ProcessFutureImpl<>(delegate.whenCompleteAsync(action), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<V> whenCompleteAsync(BiConsumer<? super V, ? super Throwable> action, Executor executor) {
+        return new ProcessFutureImpl<>(delegate.whenCompleteAsync(action, executor), getProcessAccess());
+    }
+
+    @Override
+    public ProcessFuture<V> exceptionally(Function<Throwable, ? extends V> fn) {
+        return new ProcessFutureImpl<>(delegate.exceptionally(fn), getProcessAccess());
+    }
+
+    @Override
+    public CompletableFuture<V> toCompletableFuture() {
+        return delegate.toCompletableFuture();
     }
 }
